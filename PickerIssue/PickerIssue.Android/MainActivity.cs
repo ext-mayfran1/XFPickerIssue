82d60aab0b789da9f1b6920084ce216426323af2
François-Olivier Mayette <francois-olivier.mayette@optelgroup.com>
2019-04-23 11:31:19 -0400

    Initial

---
+++ PickerIssue/PickerIssue.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace PickerIssue.Droid
+{
+    [Activity(Label = "PickerIssue", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
